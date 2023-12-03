414baab360c901d97f542d2b7619714d09f40986
helloworld877 <mmghaly3@gmail.com>
2023-12-03 21:35:34 +0200

    added backend starting files

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+var app = express();
+app.get("/", function (request, response) {
+  response.send("Hello World!");
+});
+app.listen(8080, function () {
+  console.log("Started application on port %d", 8080);
+});
