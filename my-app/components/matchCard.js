2e9f35b72668e4aa8c8197ea626d9351ef3f1e6f
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2023-12-07 13:40:39 +0200

    updated match cards to be clickable

--- my-app/components/matchCard.js
+++ my-app/components/matchCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from '../styles/matchCard.css';
 
 const MatchCard = () => {
-  
     const matchDetails = {
         homeTeam: 'Al Ahly',
         awayTeam: 'Zamalek',
@@ -12,23 +12,28 @@ const MatchCard = () => {
         linesmen: ['Mahmoud Abouelregal', 'Tahssen Bedyer']
     };
 
-   
     const homeTeamImageUrl = '/images/ahly.png'; 
     const awayTeamImageUrl = '/images/Zamalek.png'; 
+
+    const matchPageDetailsUrl = `/match-details?homeTeam=${matchDetails.homeTeam}&awayTeam=${matchDetails.awayTeam}&venue=${matchDetails.venue}&dateTime=${matchDetails.dateTime}&mainReferee=${matchDetails.mainReferee}&linesmen=${encodeURIComponent(matchDetails.linesmen.join(', '))}`;
     
     return (
-        <div className="card">
-            <div className="logos-container">
-                <img src={homeTeamImageUrl} alt={`${matchDetails.homeTeam} Logo`} className="team-logo" />
-                <div className="vs">VS</div>
-                <img src={awayTeamImageUrl} alt={`${matchDetails.awayTeam} Logo`} className="team-logo" />
+       
+            <div className="card">
+                 <Link href={matchPageDetailsUrl} className="match-link" >
+                <div className="logos-container">
+                    <img src={homeTeamImageUrl} alt={`${matchDetails.homeTeam} Logo`} className="team-logo" />
+                    <div className="vs">VS</div>
+                    <img src={awayTeamImageUrl} alt={`${matchDetails.awayTeam} Logo`} className="team-logo" />
+                </div>
+                <h3>{matchDetails.homeTeam} vs {matchDetails.awayTeam}</h3>
+                <p><strong>Venue:</strong> {matchDetails.venue}</p>
+                <p><strong>Date & Time:</strong> {matchDetails.dateTime}</p>
+                <p><strong>Main Referee:</strong> {matchDetails.mainReferee}</p>
+                <p><strong>Linesmen:</strong> {matchDetails.linesmen.join(', ')}</p>
+                </Link>
             </div>
-            <h3>{matchDetails.homeTeam} vs {matchDetails.awayTeam}</h3>
-            <p><strong>Venue:</strong> {matchDetails.venue}</p>
-            <p><strong>Date & Time:</strong> {matchDetails.dateTime}</p>
-            <p><strong>Main Referee:</strong> {matchDetails.mainReferee}</p>
-            <p><strong>Linesmen:</strong> {matchDetails.linesmen.join(', ')}</p>
-        </div>
+     
     );
 };
 
