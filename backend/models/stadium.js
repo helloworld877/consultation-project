6d51a4ade6ca213f513155fcd9a1c95fdd0695fe
Marina Sameh <Ziad.Wareth@gmail.com>
2023-12-07 20:56:49 +0200

    Added Stadium Model

---
+++ backend/models/stadium.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const stadiumSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  rows: {
+    type: Number,
+    required: true,
+  },
+  columns: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Stadium", stadiumSchema);
