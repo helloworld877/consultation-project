1ebf16d440af218669b8574fbdf219f06c38b0ac
helloworld877 <mmghaly3@gmail.com>
2023-12-07 09:36:21 +0200

    fixed routes ad added body parser

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,23 @@
+const bodyParser = require("body-parser");
+
 const express = require("express");
-const port = 8080;
+const PORT = 8080;
 const mongoose = require("mongoose");
+const app = express();
+
+//starting app and database connection
 mongoose
-  .connect("mongodb+srv://mmghaly3:EO0TUxRL6RRwEwsw@users.iaf0wje.mongodb.net/")
+  .connect("mongodb+srv://mmghaly3:4Il0wSUKzkeOu1qP@users.iaf0wje.mongodb.net/")
   .then((result) =>
-    app.listen(port, (req, res, next) => {
-      console.log("Server running on port 5000");
+    app.listen(PORT, (req, res, next) => {
+      console.log(`Server running on port ${PORT}`);
+      const userRoutes = require("./routes/userRoutes");
+      const matchRoutes = require("./routes/matchRoutes");
+      app.use("/users", userRoutes);
+      app.use("/matches", matchRoutes);
+      app.get("/", function (req, res) {
+        res.send("Hello World!");
+      });
     })
   )
   .catch((err) => console.log(err));
-const app = express();
-app.use(express.json);
-const userRoutes = require("./routes/userRoutes");
-const matchRoutes = require("./routes/matchRoutes");
-app.use("/users", userRoutes);
-app.use("/matches", matchRoutes);
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
-app.listen(port, function () {
-  console.log("Started application on port %d", 8080);
-});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const bodyParser = require("body-parser");
 
-router.get("/users", userController.getUsers); // Get all users
-router.get("/user", userController.getUser); //Get a user based on userName
-router.post("/user", userController.createUser); // Create a new user
+router.get("/getUsers", bodyParser.json(), userController.getUsers); // Get all users
+router.get("/getUser", bodyParser.json(), userController.getUser); //Get a user based on userName
+router.post("/createUser", bodyParser.json(), userController.createUser); // Create a new user
 
 module.exports = router;
