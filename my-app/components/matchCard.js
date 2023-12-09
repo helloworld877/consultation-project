97126c5b4927d69df2de3b7afa88dd48608a76cc
Sarraa Muhammad <102469790+sarraamuhammad@users.noreply.github.com>
2023-12-09 10:18:36 +0200

    Update matchCard.js

--- my-app/components/matchCard.js
+++ my-app/components/matchCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/matchCard.css';
 
-const MatchCard = () => {
+const MatchCard = ({ clickable = true })=> {
     const matchDetails = {
         homeTeam: 'Al Ahly',
         awayTeam: 'Zamalek',
@@ -15,12 +15,11 @@ const MatchCard = () => {
     const homeTeamImageUrl = '/images/ahly.png'; 
     const awayTeamImageUrl = '/images/Zamalek.png'; 
 
-    const matchPageDetailsUrl = `/match-details?homeTeam=${matchDetails.homeTeam}&awayTeam=${matchDetails.awayTeam}&venue=${matchDetails.venue}&dateTime=${matchDetails.dateTime}&mainReferee=${matchDetails.mainReferee}&linesmen=${encodeURIComponent(matchDetails.linesmen.join(', '))}`;
+    const matchPageDetailsUrl = `/matchSeats?homeTeam=${matchDetails.homeTeam}&awayTeam=${matchDetails.awayTeam}&venue=${matchDetails.venue}&dateTime=${matchDetails.dateTime}&mainReferee=${matchDetails.mainReferee}&linesmen=${encodeURIComponent(matchDetails.linesmen.join(', '))}`;
     
     return (
-       
-            <div className="card">
-                 <Link href={matchPageDetailsUrl} className="match-link" >
+        <div className={`card ${clickable ? 'clickable' : 'unclickable'}`}>
+            <a href={clickable ? matchPageDetailsUrl : undefined} className="match-link">
                 <div className="logos-container">
                     <img src={homeTeamImageUrl} alt={`${matchDetails.homeTeam} Logo`} className="team-logo" />
                     <div className="vs">VS</div>
@@ -31,9 +30,8 @@ const MatchCard = () => {
                 <p><strong>Date & Time:</strong> {matchDetails.dateTime}</p>
                 <p><strong>Main Referee:</strong> {matchDetails.mainReferee}</p>
                 <p><strong>Linesmen:</strong> {matchDetails.linesmen.join(', ')}</p>
-                </Link>
-            </div>
-     
+            </a>
+        </div>
     );
 };
 
