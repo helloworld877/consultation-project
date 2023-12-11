6f598232a4f222f01c0db5b91717b4bc510e0775
Sarraa Muhammad <sarraa.khalil01@eng-st.cu.edu.eg>
2023-12-11 23:02:59 +0200

    Added and edited login and front pages

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,210 +1,51 @@
 "use client";
 import React, { useState } from "react";
-import "../../styles/onBoarding.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import { Button, Carousel } from "react-bootstrap";
+import "../../styles/frontPage.css";
+import CustomButton from "../../components/customButton";
 
 export default function Home() {
-  const [isLogin, setIsLogin] = useState(true); // State to toggle between login and signup
-
-  const toggleForm = () => {
-    setIsLogin(!isLogin); // Toggle the state
-  };
-
-  const loginForm = () => {
-    setIsLogin(true);
-  };
-  const signUpForm = () => {
-    setIsLogin(false);
-  };
   return (
-    <div className="page-container">
-      <div className="section">
-        <div className="container">
-          <div className="row full-height justify-content-center">
-            <div className="col-12 text-center align-self-center py-5">
-              <div className="section pb-5 pt-5 pt-sm-2 text-center">
-                <div className="button-container">
-                  <Link href="/viewMatches">
-                    {/* <a className="view-matches-btn">View Matches</a>{" "} */}
-                    <button type="button" className="btn btn-primary">
-                      View Matches
-                    </button>
-                  </Link>
-                </div>
-                <input
-                  className="checkbox"
-                  type="checkbox"
-                  id="reg-log"
-                  name="reg-log"
-                />
-                <div className="toggle-container">
-                  <span className="toggle-label-text" onClick={loginForm}>
-                    Login
-                  </span>
-                  <label
-                    htmlFor="reg-log"
-                    className="toggle-label"
-                    onClick={toggleForm}
-                  >
-                    <FontAwesomeIcon
-                      icon={isLogin ? faArrowLeft : faArrowRight} // Toggle between icons
-                      className="toggle-icon"
-                    />
-                  </label>
-                  <span className="toggle-label-text" onClick={signUpForm}>
-                    Sign Up
-                  </span>
-                </div>
-
-                <div
-                  className={`card-3d-wrap mx-auto ${isLogin ? "" : "flipped"}`}
-                >
-                  <div className="card-3d-wrapper">
-                    <div className="card-front">
-                      <div className="center-wrap">
-                        <div className="section text-center">
-                          <h4 className="mb-4 pb-3">Log In</h4>
-                          <div className="form-group">
-                            <input
-                              type="email"
-                              name="logemail"
-                              className="form-style"
-                              placeholder="Your Email"
-                              id="logemail"
-                              autoComplete="off"
-                            />
-                            <i className="input-icon uil uil-at"></i>
-                          </div>
-                          <div className="form-group mt-2">
-                            <input
-                              type="password"
-                              name="logpass"
-                              className="form-style"
-                              placeholder="Your Password"
-                              id="logpass"
-                              autoComplete="off"
-                            />
-                            <i className="input-icon uil uil-lock-alt"></i>
-                          </div>
-                          <a href="#" className="btn mt-4">
-                            submit
-                          </a>
-                          <p className="mb-0 mt-4 text-center">
-                            <a href="#0" className="link">
-                              Forgot your password?
-                            </a>
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="card-back">
-                      <div className="center-wrap">
-                        <div className="section text-center">
-                          <h4 className="mb-4 pb-3">Sign Up</h4>
-                          <div className="form-row">
-                            <div className="form-column">
-                              <div className="form-group">
-                                <input
-                                  type="text"
-                                  name="username"
-                                  className="form-style"
-                                  placeholder="Username"
-                                  id="logname"
-                                  autoComplete="off"
-                                />
-                                <i className="input-icon uil uil-user"></i>
-                              </div>
-                              <div className="form-group">
-                                <input
-                                  type="text"
-                                  name="firstName"
-                                  className="form-style"
-                                  placeholder="First Name"
-                                  autoComplete="off"
-                                />
-                              </div>
-                              <div className="form-group">
-                                <input
-                                  type="text"
-                                  name="lastName"
-                                  className="form-style"
-                                  placeholder="Last Name"
-                                  autoComplete="off"
-                                />
-                              </div>
-                              <div className="form-group">
-                                <input
-                                  type="date"
-                                  name="birthDate"
-                                  className="form-style"
-                                  placeholder="Birth Date"
-                                  autoComplete="off"
-                                />
-                              </div>
-                            </div>
-                            <div className="form-column">
-                              <div className="form-group">
-                                <select
-                                  name="gender"
-                                  className="form-style gender"
-                                  defaultValue=""
-                                >
-                                  <option value="" disabled>
-                                    Gender
-                                  </option>
-                                  <option value="male">Male</option>
-                                  <option value="female">Female</option>
-                                </select>
-                              </div>
-
-                              <div className="form-group mt-2">
-                                <input
-                                  type="email"
-                                  name="logemail"
-                                  className="form-style"
-                                  placeholder="Your Email"
-                                  id="logemail"
-                                  autoComplete="off"
-                                />
-                                <i className="input-icon uil uil-at"></i>
-                              </div>
-                              <div className="form-group mt-2">
-                                <input
-                                  type="password"
-                                  name="logpass"
-                                  className="form-style"
-                                  placeholder="Your Password"
-                                  id="logpass"
-                                  autoComplete="off"
-                                />
-                                <i className="input-icon uil uil-lock-alt"></i>
-                              </div>
-                              <div className="form-group">
-                                <input
-                                  type="text"
-                                  name="address"
-                                  className="form-style"
-                                  placeholder="Address"
-                                  autoComplete="off"
-                                />
-                              </div>
-                            </div>
-                          </div>
-                          <a href="#" className="btn mt-4">
-                            submit
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+    <div>
+      <header>
+        <h1>Egyptian Premier League</h1>
+        <div className="header-buttons">
+          <div className="button-container">
+            <Link href="/login">
+              <CustomButton>Login</CustomButton>
+            </Link>
+          </div>
+          <div className="button-container">
+            <Link href="/viewMatches">
+              <CustomButton>View Matches</CustomButton>
+            </Link>
+          </div>
+          <div className="button-container">
+            <Link href="/pastMatches">
+              <CustomButton>View Past Matches</CustomButton>
+            </Link>
           </div>
         </div>
-      </div>
+      </header>
+      {/* <main>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 match-image"
+                        src="https://egyptianstreets.com/wp-content/uploads/2017/01/2011244-42220367-2560-1440.jpg"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 match-image"
+                        src="https://english.ahram.org.eg/Media/News/2023/9/18/41_2023-638306579125578465-557.jpg"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </main> */}
     </div>
   );
 }
