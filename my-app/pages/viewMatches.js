419b8ce37ff40375d95345566a51e58ccc4c2424
Sarraa Muhammad <sarraa.khalil01@eng-st.cu.edu.eg>
2023-12-11 22:03:43 +0200

    added missing buttons in viewMatches

--- my-app/pages/viewMatches.js
+++ my-app/pages/viewMatches.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import MatchCard from '../components/matchCard';
-import '../styles/viewMatches.css';
-
-
+import React from "react";
+import MatchCard from "../components/matchCard";
+import "../styles/viewMatches.css";
+import CustomButton from "../components/customButton";
+import Link from "next/link";
 
 export default function Matches() {
-   
-    return (
-        
-        <div className="matches-page">
-            <h1>Upcoming Matches</h1>
-            <MatchCard showEditIcon={true}  />
-        </div>
-    );
+  return (
+    <div className="matches-page">
+      <h1>Upcoming Matches</h1>
+      <div className="add-match-button">
+        <Link href="/addMatch">
+        <CustomButton>
+          + Add Match
+        </CustomButton>
+        </Link>
+      </div>
+      <div className="add-stadium-button">
+        <Link href="/addStadium">
+        <CustomButton>
+          + Add Stadium
+        </CustomButton>
+        </Link>
+      </div>
+      <MatchCard showEditIcon={true} />
+    </div>
+  );
 }
-
-
-
 
 // import React, { useEffect, useState } from 'react';
 
