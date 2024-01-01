2045b77e5890ac51cd38868d8f9dead9d8a69718
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-01-02 01:35:36 +0200

    trying log out

--- my-app/pages/profile.js
+++ my-app/pages/profile.js
@@ -3,7 +3,8 @@ import Link from "next/link";
 import "../styles/profile.css";
 
 export default function Profile() {
- 
+
+  const [logoutError, setLogoutError] = useState(""); 
   const [details, setDetails] = useState({
     userName: "",
     firstName: "",
@@ -16,15 +17,13 @@ export default function Profile() {
     city: "",
   });
 
-
   const userProfileImage = "/images/football.jpg";
   useEffect(() => {
     fetchDetails();
   }, []);
 
-  
-    console.log('HA FETCHH');
-    
+  console.log("HA FETCHH");
+
   const fetchDetails = async (e) => {
     try {
       const accessToken = localStorage.getItem("token");
@@ -68,10 +67,51 @@ export default function Profile() {
       console.error("There was an error fetching the account details:", error);
     }
   };
-    
+
+  const logout = async (e) => {
+    console.log("HAMSA7777");
+    try {
+      const accessToken = localStorage.getItem("token");
+      const response = await fetch("http://localhost:8080/users/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `bearer ${accessToken}`,
+        },
+      });
+      console.log(`bearer ${accessToken}`);
+
+
+      if (!response.ok) {
+        throw new Error("Logout failed");
+      }
+
+      const result = await response.json();
+      console.log("RESULT BTA3 LOG OUT");
+      console.log(result);
+      console.log(result.message);
+      if(result.message === "Logged out successfully")
+      {
+        localStorage.removeItem("token");
+        router.push("/");
+      } else
+      {
+        setLogoutError("Not logged out. Please try again."); 
+      }
+    } catch (error) {
+      console.error("Error logging out:", error);
+      setLogoutError("An error occurred. Please try again."); 
+    }
+  };
+
   return (
     <div className="profile-page">
       <h1>Profile</h1>
+      {logoutError && ( // Display the alert if there is a logout error
+        <div className="alert alert-danger" role="alert">
+          {logoutError}
+        </div>
+      )}
       <div className="user-info">
         <div className="user-details">
           <img src={userProfileImage} alt="Profile" className="profile-image" />
@@ -92,9 +132,9 @@ export default function Profile() {
           <Link href="/reservations" passHref>
             <h1 className="link reservations">View Tickets</h1>
           </Link>
-          <Link href="/" passHref>
-            <h1 className="link logOut">Log Out</h1>
-          </Link>
+          <button onClick={logout} className="link logOut">
+            Log Out
+          </button>
         </div>
       </div>
     </div>
