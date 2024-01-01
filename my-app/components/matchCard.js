9bc9417baff8d2df2130294cfb66574313e2835f
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-01-01 14:52:38 +0200

    trying to connect to backend without testing

--- my-app/components/matchCard.js
+++ my-app/components/matchCard.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
 const MatchCard = ({
+  _id,
   homeTeam,
   awayTeam,
   matchVenue,
@@ -27,29 +28,29 @@ const MatchCard = ({
   };
 
   const onEdit = () => {
-    const editUrl = `/editMatch?homeTeam=${encodeURIComponent(
-      matchDetails.homeTeam
-    )}&awayTeam=${encodeURIComponent(
-      matchDetails.awayTeam
-    )}&venue=${encodeURIComponent(
-      matchDetails.venue
-    )}&dateTime=${encodeURIComponent(
-      matchDetails.dateTime
-    )}&mainReferee=${encodeURIComponent(
-      matchDetails.mainReferee
-    )}&linesmen=${encodeURIComponent(matchDetails.linesmen.join(", "))}`;
+    const editUrl =
+      `/editMatch?` +
+      `id=${encodeURIComponent(matchDetails._id)}&` +
+      `homeTeam=${encodeURIComponent(matchDetails.homeTeam)}&` +
+      `awayTeam=${encodeURIComponent(matchDetails.awayTeam)}&` +
+      `venue=${encodeURIComponent(matchDetails.venue)}&` +
+      `dateTime=${encodeURIComponent(matchDetails.dateTime)}&` +
+      `mainReferee=${encodeURIComponent(matchDetails.mainReferee)}&` +
+      `linesmen=${encodeURIComponent(matchDetails.linesmen.join(", "))}`;
+
     router.push(editUrl);
   };
+
   const date = new Date(dateAndTime);
   const formattedDate = date.toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",
     day: "numeric",
-  }); // Adjust the locale and options as needed
+  });
   const formattedTime = date.toLocaleTimeString("en-US", {
     hour: "2-digit",
     minute: "2-digit",
-  }); // Adjust the locale and options as needed
+  });
 
   const homeTeamImageUrl = "/images/ahly.png";
   const awayTeamImageUrl = "/images/Zamalek.png";
