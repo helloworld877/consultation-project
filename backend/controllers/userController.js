e8ceef492a4506394333b1e25b0c36df8d7b6ca4
Marina Sameh <Ziad.Wareth@gmail.com>
2024-01-02 00:32:14 +0200

    Added Logout and Get All Sign Up Requests

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,9 @@ const authenticateToken = (req, res, next) => {
   if (token === null) {
     return res.sendStatus(401);
   }
-
+  if (tokenBlacklist.has(token)) {
+    return res.status(401).json({ error: "Token has been logged out" });
+  }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) {
       return res.sendStatus(403);
@@ -219,7 +221,6 @@ const login = (req, res, next) => {
         const user = { result };
         console.log(user);
         const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-
         //return user data and access token
         res.status(200).json({
           message: "Login Successful",
@@ -259,6 +260,7 @@ const forgotPassword = (req, res, next) => {
   }
 };
 
+//Change Password from Profile Page
 const changePassword = (req, res, next) => {
   const oldPassword = req.body.password;
   const newPassword = req.body.newPassword;
@@ -289,7 +291,33 @@ const changePassword = (req, res, next) => {
     res.status(500).json({ message: "Password Mismatch" });
   }
 };
+//Checking All SignUp requests
+const getAllRequests = (req, res, next) => {
+  console.log(req.USER.result.role);
+  if (req.USER.result.role !== "Manager") {
+    return res.status(403).json({ error: "not authorized" });
+  }
+  User.find({ isConfirmed: 1 }) // Only find users where isConfirmed is 1 (JUST SIGNED)
+    .then((users) => {
+      console.log(users);
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.log("Couldn't get users");
+      console.log(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+};
+//LogOut Functionality
+const tokenBlacklist = new Set();
+const logout = (req, res) => {
+  const token = req.headers["authorization"].split(" ")[1];
 
+  // Ideally, you would store this in a persistent datastore.
+  tokenBlacklist.add(token);
+
+  res.status(200).json({ message: "Logged out successfully" });
+};
 module.exports = {
   getUsers,
   getUser,
@@ -300,7 +328,9 @@ module.exports = {
   updateUser,
   deleteUser,
   login,
+  logout,
   authenticateToken,
   forgotPassword,
   changePassword,
+  getAllRequests,
 };
