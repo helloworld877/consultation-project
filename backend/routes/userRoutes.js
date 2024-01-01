dd17149964517c6eb68e6d2a675a803dbf12f68b
Marina Sameh <Ziad.Wareth@gmail.com>
2024-01-01 20:08:02 +0200

    Adding Token To Get User

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,7 +5,12 @@ const bodyParser = require("body-parser");
 
 // Authentication
 router.get("/getUsers", bodyParser.json(), userController.getUsers); // Get all users
-router.get("/getUser", bodyParser.json(), userController.getUser); //Get a user based on userName
+router.get(
+  "/getUser",
+  bodyParser.json(),
+  userController.authenticateToken,
+  userController.getUser
+); //Get a user based on userName
 router.post("/createUser", bodyParser.json(), userController.createUser); // Create a new user
 router.put(
   "/updateUser",
