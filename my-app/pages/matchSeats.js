68f2068b1423c7acdc157d2d00147eaf7a8be816
Sarraa Muhammad <sarraa.khalil01@eng-st.cu.edu.eg>
2024-01-02 03:42:31 +0200

    integrated with backend

--- my-app/pages/matchSeats.js
+++ my-app/pages/matchSeats.js
@@ -10,17 +10,30 @@ export default function Seats() {
   const [matchDetails, setMatchDetails] = useState({});
   const [reservedSeats, setReservedSeats] = useState([]);
   const [selectedSeats, setSelectedSeats] = useState([]);
+  const [stadiumSize, setStadiumSize] = useState([]);
   const router = useRouter();
 
   useEffect(() => {
     const matchId = router.query.matchID;
-    // Make sure to use template literals correctly with backticks
     fetch(`http://localhost:8080/matches/getMatch/${matchId}`)
       .then((response) => response.json())
       .then((data) => {
         setMatchDetails(data);
         setReservedSeats(data.reservedSeats || []);
-        console.log(matchDetails);
+        console.log(data.reservedSeats);
+        let size = data.reservedSeats.length;
+        let allReservedSeats = new Array(size);
+        if (data.reservedSeats) {
+          for (let i = 0; i < data.reservedSeats.length; i++) {
+            const row = data.reservedSeats[i][0];
+            const col = data.reservedSeats[i][1];
+            const seatId = `${row}-${col}`;
+            allReservedSeats.push(seatId);
+          }
+        }
+        console.log(allReservedSeats);
+        setReservedSeats(allReservedSeats);
+        setStadiumSize(data.size || []);
       })
       .catch((error) => {
         console.error("Error fetching match details:", error);
@@ -30,12 +43,10 @@ export default function Seats() {
   const matchPageDetailsUrl = `/Checkout?matchID=${router.query.matchID}`;
 
   const handleSeatToggle = (row, col) => {
-    // Use template literals with backticks correctly
     const seatId = `${row}-${col}`;
     const isSelected = selectedSeats.includes(seatId);
-    const isReserved = reservedSeats.some(
-      (seat) => seat.row === row && seat.col === col
-    );
+    const isReserved = reservedSeats.includes(seatId);
+
     if (isReserved) return;
 
     if (isSelected) {
@@ -52,24 +63,24 @@ export default function Seats() {
           <div className="col-12 text-center align-self-center py-5">
             <div className="section pb-5 pt-5 pt-sm-2 text-center">
               <header>
-              <div className="button-container">
-                <Link href={matchPageDetailsUrl} passHref>
-                  <button type="button" className="checkout-btn">
-                    Checkout
-                  </button>
-                </Link>
-              </div>
-              <Link href="/viewMatches" passHref>
-                <div className="back-btn-container">
-                  <label htmlFor="reg-log" className="back-btn-label">
-                    <FontAwesomeIcon
-                      icon={faArrowLeft}
-                      className="back-btn-icon"
-                    />
-                  </label>
-                  <span className="back-btn-label-text">Return To Matches</span>
+                <div className="button-container">
+                  <Link href={matchPageDetailsUrl} passHref>
+                    <button type="button" className="checkout-btn">
+                      Checkout
+                    </button>
+                  </Link>
                 </div>
-              </Link>
+                <Link href="/viewMatches" passHref>
+                  <div className="back-btn-container">
+                    <label htmlFor="reg-log" className="back-btn-label">
+                      <FontAwesomeIcon
+                        icon={faArrowLeft}
+                        className="back-btn-icon"
+                      />
+                    </label>
+                    <span className="back-btn-label-text">Return To Matches</span>
+                  </div>
+                </Link>
               </header>
               <div className="card-container">
                 <MatchCard
@@ -83,53 +94,29 @@ export default function Seats() {
                   showEditIcon={false}
                 />
               </div>
-              <div className="card-3d-wrap mx-auto">
-                <div className="card-3d-wrapper">
-                  <div className="card-front">
-                    <div className="center-wrap">
-                      <div className="section text-center">
-                        <div className="form-row">
-                          {[...Array(5)].map((_, row) => (
-                            <div key={row} className="seat-row">
-                              {[...Array(4)].map((_, col) => {
-                                const seatId = `${row}-${col}`;
-                                return (
-                                  <button
-                                    key={seatId}
-                                    className={`seat-btn ${
-                                      selectedSeats.includes(seatId)
-                                        ? "selected"
-                                        : ""
-                                    } ${
-                                      reservedSeats.some(
-                                        (seat) =>
-                                          seat.row === row && seat.col === col
-                                      )
-                                        ? "reserved"
-                                        : ""
-                                    }`}
-                                    onClick={() => handleSeatToggle(row, col)}
-                                    disabled={reservedSeats.some(
-                                      (seat) =>
-                                        seat.row === row && seat.col === col
-                                    )}
-                                  >
-                                    {String.fromCharCode(65 + row)}
-                                    {col + 1}
-                                  </button>
-                                );
-                              })}
-                            </div>
-                          ))}
-                        </div>
-
-                        <div className="form-row">
-                          <button className="field-btn">Stadium Field</button>
-                        </div>
-                      </div>
-                    </div>
+              <div className="seats">
+                {[...Array(stadiumSize[0])].map((_, row) => (
+                  <div key={row} className="seat-row">
+                    {[...Array(stadiumSize[1])].map((_, col) => {
+                      const seatId = `${row}-${col}`;
+                      console.log(seatId);
+                      const isReserved = reservedSeats.includes(seatId);
+                      return (
+                        <button
+                          key={seatId}
+                          className={`seat-btn ${
+                            selectedSeats.includes(seatId) ? "selected" : ""
+                          } ${isReserved ? "reserved" : ""}`}
+                          onClick={() => handleSeatToggle(row, col)}
+                          disabled={isReserved}
+                        >
+                          {String.fromCharCode(65 + row)}
+                          {col + 1}
+                        </button>
+                      );
+                    }).reverse()}
                   </div>
-                </div>
+                ))}
               </div>
             </div>
           </div>
