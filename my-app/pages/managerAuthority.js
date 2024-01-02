0aa1e5fd7e8988a1d9178d298a6620b5eac829c1
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-01-02 02:22:14 +0200

    trying to access array if requests

--- my-app/pages/managerAuthority.js
+++ my-app/pages/managerAuthority.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "../styles/managerAuthority.css";
 
-export async function getStaticProps() {
+export default function Admin() {
+  const [existingManagers, setExistingManagers] = useState([]);
+  const [managerRequests, setManagerRequests] = useState([]);
+
+  useEffect(() => {
+    console.log("ana d5alt ahw hgeb al arrays");
+    const accessToken = localStorage.getItem("token");
+    async function fetchManagerDetails() {
+      try {
+        const response = await fetch('http://localhost:8080/users/getSignUpRequests', {
+          headers: {
+            authorization: `bearer ${accessToken}`,
+          }
+        });
+        console.log(`bearer ${accessToken}`);
+
+        if (response.data) {
+          setExistingManagers(response.data.managers);
+          setManagerRequests(response.data.confirmedUsers);
+          console.log(response.data.managers);
+          console.log(response.data.confirmedUsers);
+        }
+      } catch (error) {
+        console.error("There was an error!", error);
+      }
+    }
  
-  const existingManagers = ['Marly Mofeed'];
-  const managerRequests = ['Sarraa', 'Mark Milad', 'Ziad'];
+    console.log(existingManagers);
+    console.log(managerRequests);
 
-  return {
-    props: {
-      existingManagers,
-      managerRequests,
-    },
-    revalidate: 10,
-  };
-}
+    fetchManagerDetails();
+  }, []);
 
-export default function Admin({ existingManagers, managerRequests }) {
-  
   return (
     <div className="container">
       <h1>Admin view</h1>
       <div className="listSection">
         <h2>Manager Details</h2>
         <ul className="managerList">
-          {existingManagers.map((manager) => (
-            <li key={manager}>
-              {manager}
+          {existingManagers.map(manager => (
+            <li key={manager._id}>
+              {manager.firstName} {manager.lastName}
               <button className="removeButton">Remove</button>
             </li>
           ))}
@@ -34,9 +51,9 @@ export default function Admin({ existingManagers, managerRequests }) {
       <div className="requestSection">
         <h2>Join us & Become a Manager</h2>
         <ul className="managerList">
-          {managerRequests.map((request) => (
-            <li key={request}>
-              <div><span>{request}</span></div>
+          {managerRequests.map(request => (
+            <li key={request._id}>
+              {request.firstName} {request.lastName}
               <button className="acceptButton">Accept</button>
               <button className="declineButton">Decline</button>
             </li>
